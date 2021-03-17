f2e952845bd2d4905eb10058de602b52cda0ce36
Sebastian Gramlich <SebastianGramlich@web.de>
2021-03-17 21:53:29 +0100

    debugging goodbye

--- script.js
+++ script.js
@@ -52,14 +52,13 @@ for (name in names) {
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
   var firstLetter = names[name].charAt(0);
-  firstLetter.toLowerCase();
-
+  
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (firstLetter == 'j') {
+  if (firstLetter.toLowerCase() === 'j') {
     byeSpeaker.speak(names[name]);
   } else {
     helloSpeaker.speak(names[name]);
