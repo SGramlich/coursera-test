13c4501207c0efc5764ec09fa2144565106ce981
Sebastian Gramlich <SebastianGramlich@web.de>
2021-03-26 18:41:39 +0100

    console.log added

--- js/script.js
+++ js/script.js
@@ -102,6 +102,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+      console.log(chosenCategoryShortName);
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -116,7 +117,9 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       var html = homeHtml;
+      console.log(html);
       var homeHtmlToInsertIntoMainPage = insertProperty(html, "short_name", chosenCategoryShortName);
+      console.log(homeHtmlToInsertIntoMainPage);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
